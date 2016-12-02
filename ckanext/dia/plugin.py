cc1e096252dc0e3577ecfdeb3fe839bd0330beba
Tobias Schulmann <tobi@catalyst.net.nz>
2016-12-02 14:36:33 +1300

    Overriding extra_key_not_in_root_schema validator

--- ckanext/dia/plugin.py
+++ ckanext/dia/plugin.py
@@ -13,14 +13,14 @@ class DIAValidationPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IValidators)
 
     def update_config(self, config):
-        # monkeypatching isodate validator to add `theme` key.
+        # monkeypatching isodate and extra_key_not_in_root_schema validators
         ckan.logic.validators.isodate = validators.isodate
+        ckan.logic.validators.extra_key_not_in_root_schema = validators.extra_key_not_in_root_schema
 
     def get_validators(self):
         return {
             'force_lower': validators.force_lower,
             'natural_num_or_missing': validators.natural_num_or_missing,
-            'extra_key_not_in_root_schema': validators.extra_key_not_in_root_schema
         }
 
 
